1cb6cb5c83306d43acb8ae0537a37a127380da74
Sheng Chuan (Michael) Hsu <cool96482@hotmail.com>
2021-01-03 14:40:34 +0800

    Updates

---
+++ precache-manifest.48b8cbaae6c80949beea554fe34fa2fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8984deb79a304facfd17fde305541e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebfc0292d0e55fe37fb",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebfc0292d0e55fe37fb",
+    "url": "/static/js/main.6363e40e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "fd0bad35762c8b29375c9ee1cefd8ca3",
+    "url": "/static/media/michaelhsu.fd0bad35.jpg"
+  },
+  {
+    "revision": "a1e92425e5103276b7bee714873286d0",
+    "url": "/static/media/resume.a1e92425.pdf"
+  }
+]);
